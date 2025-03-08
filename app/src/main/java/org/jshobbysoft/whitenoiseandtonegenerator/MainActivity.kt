1f4d9613c47c365eb5ea305c236e1fc70864e39c
jason <jshobbysoft@gmail.com>
2025-03-08 08:35:40 -0600

    Update to latest target SDK and versions

--- app/src/main/java/org/jshobbysoft/whitenoiseandtonegenerator/MainActivity.kt
+++ app/src/main/java/org/jshobbysoft/whitenoiseandtonegenerator/MainActivity.kt
@@ -36,6 +36,7 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -189,7 +190,7 @@ class MainActivity : ComponentActivity() {
                                 val initNoisePct =
                                     DataStoreManager(LocalContext.current).getFromDataStoreNP()
                                 var noisePct by remember {
-                                    mutableStateOf(
+                                    mutableFloatStateOf(
                                         initNoisePct.toString().toFloat()
                                     )
                                 }
